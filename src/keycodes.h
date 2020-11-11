8d807cb212517915af34b1017e42635716338025
Jonas Minnberg <jonas@minnberg.se>
2020-11-11 21:12:12 +0100

    Missing file

---
+++ src/keycodes.h
@@ -0,0 +1,32 @@
+#pragma once
+enum
+{
+    KEY_BACKSPACE = 1,
+    KEY_LEFT,
+    KEY_UP,
+    KEY_RIGHT,
+    KEY_DOWN,
+    KEY_PAGEUP,
+    KEY_PAGEDOWN,
+    KEY_HOME,
+    KEY_END,
+    KEY_ENTER,
+    KEY_TAB,
+    KEY_DELETE,
+
+    KEY_F1,
+    KEY_F2,
+    KEY_F3,
+    KEY_F4,
+    KEY_F5,
+    KEY_F6,
+    KEY_F7,
+    KEY_F8,
+
+    KEY_ESCAPE = 0x1b,
+
+    KEY_UNKNOWN = 0x1fffe,
+    KEY_TIMEOUT = 0x1ffff
+
+};
+
