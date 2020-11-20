059cac1125dae1367d8097d06d9ba08997902643
Jonas Minnberg <jonas@minnberg.se>
2020-11-20 15:05:46 +0100

    Missing files

---
+++ src/petscii.h
@@ -0,0 +1,8 @@
+#include <cstdint>
+
+int32_t pet2uni_up(uint8_t p);
+int32_t pet2uni_lo(uint8_t p);
+uint8_t sc2pet(uint8_t p);
+uint8_t pet2sc(uint8_t p);
+int32_t sc2uni_up(uint8_t sc);
+int32_t sc2uni_lo(uint8_t sc);
