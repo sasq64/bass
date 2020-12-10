df8da099f3038ada77762adb6b291db8488f8bf8
Jonas Minnberg <sasq64@gmail.com>
2020-12-10 21:20:22 +0100

    Experimental windows console output

--- external/ansi/win_terminal.h
+++ external/ansi/win_terminal.h
@@ -3,14 +3,55 @@
 #include "terminal.h"
 #include <coreutils/log.h>
 
+#include <conio.h>
 #include <io.h>
-#include <cstring>
+#include <windows.h>
 
+#include <cstring>
 
 namespace bbs {
 
 struct LocalTerminal : public Terminal
 {
+    CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+    HANDLE hOut;
+    DWORD savedMode = 0;
+
+    int width() const override { return csbiInfo.dwSize.X; }
+
+    int height() const override { return csbiInfo.dwSize.Y; }
+
+    bool init()
+    {
+        hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        if (hOut == INVALID_HANDLE_VALUE) {
+            return false;
+        }
+
+        if (!GetConsoleMode(hOut, &savedMode)) {
+            return false;
+        }
+
+        if (!SetConsoleMode(hOut, savedMode |
+                                      ENABLE_VIRTUAL_TERMINAL_PROCESSING
+                                      )) {
+            return false;
+        }
+
+        GetConsoleScreenBufferInfo(hOut, &csbiInfo);
+        return true;
+    }
+
+    void open() override { init(); }
+
+    void close() override
+    {
+        if (!SetConsoleMode(hOut, savedMode)) {
+            return;
+        }
+    }
+
     size_t write(std::string_view source) override
     {
         return _write(_fileno(stdout), &source[0], source.length());
@@ -19,13 +60,13 @@ struct LocalTerminal : public Terminal
     bool read(std::string& target) override
     {
         auto size = target.capacity();
-        if(size <= 0) {
+        if (size <= 0) {
             size = 8;
             target.resize(size);
         }
-        auto rc = _read(_fileno(stdin), &target[0], size-1);
-        if(rc == 0) return false;
-        if(rc < 0) {
+        auto rc = _read(_fileno(stdin), &target[0], size - 1);
+        if (rc == 0) return false;
+        if (rc < 0) {
             throw std::exception();
         }
         target[rc] = 0;
